f15b4790ade8951b4f71ba685d4ab0887d0d3bcd
yuan1986 <292549001@qq.com>
2022-10-09 23:12:58 +0800

    s

--- apiLearn/routes/follow.js
+++
@@ -1,7 +0,0 @@
-let Follow = require('../controllers/FollowController');
-var express = require('express');
-var router = express.Router();
-
-router.post('/follow', Follow.follow);
-
-module.exports = router;
--- apiLearn/routes/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var cate = require('../controllers/cateController');
-
-router.get('/', cate.getCate);
-
-module.exports = router;
